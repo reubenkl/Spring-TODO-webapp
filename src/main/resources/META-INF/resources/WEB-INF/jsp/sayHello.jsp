a378f0795a219ee6bbed93c14f00c9d59997079e
reubenkl <reubenkl1602@gmail.com>
2024-06-26 23:37:47 +0530

    First Commit

---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <h1>Welcome-JSP</h1>
+    </body>
+</html>
---
+++ src/test/java/com/reuben/springboot/TodoWebApp/TodoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reuben.springboot.TodoWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
